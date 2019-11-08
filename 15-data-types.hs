9c241f391e01962c55757fb336cb963b445e5811
Daniel <daniel@usemarkup.com>
2019-11-08 16:40:11 +0000

    save 15 / intro 16

--- 15-data-types.hs
+++ 15-data-types.hs
@@ -1,31 +1,61 @@
 
-data Anniversary = Birthday String Int Int Int       --name, year, month, day
-                | Wedding String String Int Int Int -- spouse name 1, spouse name 2, year, month, day
+data OldAnniversary = OldBirthday String Int Int Int       --name, year, month, day
+                | OldWedding String String Int Int Int -- spouse name 1, spouse name 2, year, month, day
 
-johnSmith :: Anniversary
-johnSmith = Birthday "John Smith" 1950 10 11
+oldJohnSmith :: OldAnniversary
+oldJohnSmith = OldBirthday "John Smith" 1950 10 11
 
-smithsWedding :: Anniversary
-smithsWedding = Wedding "Florence" "Gal" 1960 11 12
+oldSmithsWedding :: OldAnniversary
+oldSmithsWedding = OldWedding "Florence" "Gal" 1960 11 12
 
-anniversariesOfJohnSmith ::[Anniversary]
-anniversariesOfJohnSmith = [johnSmith, smithsWedding]
+oldAnniversariesOfJohnSmith ::[OldAnniversary]
+oldAnniversariesOfJohnSmith = [oldJohnSmith, oldSmithsWedding]
 --or declared like this
-anniversariesOfJohnSmith2 = [Birthday "John Smith" 1950 10 11, Wedding "Florence" "Gal" 1960 11 12]
+anniversariesOfJohnSmith2 = [OldBirthday "John Smith" 1950 10 11, OldWedding "Florence" "Gal" 1960 11 12]
 
-showDate :: Int -> Int -> Int -> String
-showDate y m d = show y ++ " " ++ show m ++ " " ++ show d
+oldShowDate :: Int -> Int -> Int -> String
+oldShowDate y m d = show y ++ " " ++ show m ++ " " ++ show d
 
-showAnniversary :: Anniversary -> String
-showAnniversary (Birthday name year month day) = 
-    name ++ " born " ++ showDate year month day
+oldShowAnniversary :: OldAnniversary -> String
+oldShowAnniversary (OldBirthday name year month day) = 
+    name ++ " born " ++ oldShowDate year month day
 
-showAnniversary (Wedding name1 name2 year month day) =
-    name1 ++ " married " ++ name2 ++ " on " ++ showDate year month day
+oldShowAnniversary (OldWedding name1 name2 year month day) =
+    name1 ++ " married " ++ name2 ++ " on " ++ oldShowDate year month day
      
 --types
-type Name = String
-user :: Name
+type OldName = String
+user :: OldName
 user = "Daniel"
 
 type AnniversaryRecord = [Anniversary]
+
+--complete one::
+type Name = String
+
+data Anniversary = 
+    Birthday Name Date
+    | Wedding Name Name Date
+
+data Date = Date Int Int Int -- Year, Month, Day
+johnSmith :: Anniversary
+              
+johnSmith = Birthday "John Smith" (Date 1968 7 3)
+
+smithWedding :: Anniversary
+
+smithWedding = Wedding "John Smith" "Jane Smith" (Date 1987 3 4)
+
+type AnniversaryBook = [Anniversary] 
+
+anniversariesOfJohnSmith :: AnniversaryBook
+anniversariesOfJohnSmith = [johnSmith, smithWedding]
+
+showDate :: Date -> String
+showDate (Date y m d) = show y ++ "-" ++ show m ++ "-" ++ show d
+
+showAnniversary :: Anniversary -> String
+showAnniversary (Birthday name date) =
+   name ++ " born " ++ showDate date
+showAnniversary (Wedding name1 name2 date) =
+   name1 ++ " married " ++ name2 ++ " on " ++ showDate date
